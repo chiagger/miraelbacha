6c21dd97bb5c2448904d491ae55070d326cb8ac0
chiagger <busca.chiara.cb@gmail.com>
2025-04-11 16:56:50 +0200

    contacts page working

--- src/app/Contacts/page.tsx
+++ src/app/Contacts/page.tsx
@@ -1,3 +1,80 @@
+import styles from "../page.module.css";
+import Image from "next/image";
+
+import mandyIcon from "/img/mandy.svg";
+import instagramIcon from "/img/instagram.png";
+import linkedinIcon from "/img/linkedin.png";
+
 export default function Contacts() {
-  return <div></div>;
+  return (
+    <div className={styles.page} style={{ color: "#000" }}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 5,
+          alignItems: "center",
+          marginTop: "5vh",
+        }}
+      >
+        <div style={{ fontSize: 24, fontWeight: 500 }}>Work Email</div>
+        <div>miraelbacha.eb@gmail.com</div>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 5,
+          marginTop: "5vh",
+          alignItems: "center",
+        }}
+      >
+        <div style={{ fontSize: 24, fontWeight: 500 }}>Phone</div>
+        <div>+44 7884 177270</div>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          gap: 20,
+          marginTop: "7vh",
+          width: "100%",
+          justifyContent: "center",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: 5,
+            alignItems: "center",
+          }}
+        >
+          <Image src="/img/mandy.svg" alt="" width={40} height={40}></Image>{" "}
+          <div>Mandy</div>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: 5,
+            alignItems: "center",
+          }}
+        >
+          <Image src="/img/ig.png" alt="" width={40} height={40}></Image>{" "}
+          <div>Instagram</div>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: 5,
+            alignItems: "center",
+          }}
+        >
+          <Image src="/img/in.png" alt="" width={40} height={40}></Image>{" "}
+          <div>LinkedIn</div>
+        </div>
+      </div>
+    </div>
+  );
 }
